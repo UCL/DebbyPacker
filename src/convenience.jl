819597c9419ffb96afa7752896df8c278bc6a79f
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-05-22 17:17:09 +0100

    Function to normalise where the package is built

--- src/convenience.jl
+++ src/convenience.jl
@@ -3,6 +3,10 @@ package_name(package::Package) = package_name(package.name, package.version)
 package_name(package::String, version::VersionNumber) = "$package-$version"
 # Creates standard work-directory name
 builddir(package::Package, workdir::String) = abspath(joinpath(workdir, package_name(package)))
+# Directory where the package is located with debian files and such
+function packagedir(package::Package, workdir::String)
+  joinpath(builddir(package, workdir), package_name(package))
+end
 # Creates standard tar-file name
 function tarfile(package::Package, workdir::String)
   joinpath(builddir(package, workdir), "$(package.name)_$(package.version).orig.tar.gz")
